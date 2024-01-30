6db13e607d39ea356f11c75144b12f6dbb203304
yeray <yeray.programacion.web@gmail.com>
2024-01-30 00:06:36 +0000

    caja y animacion

--- Game/caja.js
+++ Game/caja.js
@@ -1,88 +1,76 @@
-// class Caja {
-//     // Constructor que inicializa las propiedades de la caja
-//     constructor(ctx) {
-//         this.ctx = ctx;
-//         this.x = 200; // Posición inicial en el eje X
-//         this.y = 200; // Posición inicial en el eje Y
-//         this.caja = new Image(); // Instancia de la clase Image para el sprite
-//         this.caja.src = "../Public/img/cajaA.png"; // Establece la ruta del sprite
-//         this.spriteFrameWidth = 20; // Ancho de cada frame en el sprite
-//         this.spriteFrameHeight = 20; // Altura de cada frame en el sprite
-//         this.totalFramesPerRow = 4; // Número total de frames por fila en el sprite (4)
-//         this.numFilas = 3; // Número total de filas en el sprite (3) totalRows
-//         this.totalFrames = this.totalFramesPerRow * this.numFilas; // Cálculo del número total de frames en el sprite (4*3=12)
-//         this.currentFrame = 0; // Número de frame actual (inicia en 0)
+class Caja {
+    // Constructor que inicializa las propiedades de la caja
+    constructor(ctx, x, y) {
+        this.ctx = ctx;
+        this.x = x || 50; // Posición inicial en el eje X
+        this.y = y || 50; // Posición inicial en el eje Y
+        this.w = 40; // Ancho de cada frame en el sprite
+        this.h = 40; // Altura de cada frame en el sprite
 
-//     }
+        this.caja = new Image(); // Instancia de la clase Image para el sprite
+        this.caja.src = "../Public/img/cajaA.png"; // Establece la ruta del sprite
 
+        this.countW = 0;
+        this.countH = 0;
+        this.tick = 0; // contador de tiempo
 
-//     animacion() {
-//         // Incrementa el frame actual (puedes ajustar la velocidad de la animación cambiando el valor 0.2)
-//         this.currentFrame += 0.2;
+        this.dispose = true;
+        this.colliding = true;
+        
+    }
 
-//         // Si hemos llegado al final del sprite, reinicia el frame
-//         if (this.currentFrame >= this.totalFrames) {
-//             this.currentFrame = 0;
-//         }
-//     }
+    draw() { 
+        this.ctx.drawImage(
+            this.caja,     // Imagen del sprite
+            (this.countW * this.caja.width) / 4,
+            (this.countH * this.caja.height) / 3,
+            this.caja.width / 4,
+            this.caja.height / 3,
+            this.x,        // Posición X en el canvas
+            this.y,        // Posición Y en el canvas
+            this.w,        // Ancho del frame en el canvas
+            this.h         // Altura del frame en el canvas
+        );
+        // console.log(JSON.stringify(this.ctx.drawImage, null, 2)); //Esto imprimirá el objeto de manera más legible en la consola con una sangría de 2 espacios.
+    }
 
+    move() {
+        if(this.colliding){
+        //va recorriendo el eje x 
+        this.tick ++;
+        if(this.tick > 3) {
+            this.countW ++;
+            this.tick = 0;
+        }
 
-//     draw() { // Dibuja el sprite en el canvas utilizando el ctx 2D
-
-//         // Calcula la fila del frame actual dividiendo el índice del frame actual por el número de columnas en el sprite
-//         const row = Math.floor(this.currentFrame / 4); // 0/4 = 0 fila 0 como los indices de un array
-//         // Calcula la columna del frame actual tomando el resto de la división del índice del frame actual por el número de columnas
-//         const col = this.currentFrame % 4; // 0%4 = 0 columna 0 como los indices de un array
-
-//         //?En caso de que no funcione arriba descomentar este y comentar el otro.
-//         // const row = Math.floor(this.currentFrame / 4); // 4 frames por fila
-//         // const col = this.currentFrame % 3; // 3 frames por columna
-
-
-//         // Dibuja el frame actual del sprite en el canvas
-//         this.ctx.drawImage(
-//             this.caja,              // Imagen del sprite
-//             col * this.spriteFrameWidth,  // Posición X en el sprite (multiplica la columna por el ancho del frame) 0*20 = 0
-//             row * this.spriteFrameHeight, // Posición Y en el sprite (multiplica la fila por la altura del frame) 0*20 = 0
-//             this.spriteFrameWidth,        // Ancho del frame del sprite
-//             this.spriteFrameHeight,       // Altura del frame del sprite
-//             this.x,                       // Posición X en el canvas
-//             this.y,                       // Posición Y en el canvas
-//             this.canvasFrameWidth,        // Ancho del frame en el canvas
-//             this.canvasFrameHeight         // Altura del frame en el canvas
-//         );
-//         console.log(JSON.stringify(this.ctx.drawImage, null, 2)); //Esto imprimirá el objeto de manera más legible en la consola con una sangría de 2 espacios.
-
-//     }
-
-//         // draw() {
-//         //     // Calcular las coordenadas de textura
-//         //     const fila = Math.floor(this.frameActual / this.numColumnas);
-//         //     const columna = this.frameActual % this.numColumnas;
-
-//         //     // Dibujar la subimagen actual del sprite en el canvas
-//         //     this.ctx.drawImage(
-//         //         this.caja,
-//         //         columna * this.frameWidth, fila * this.frameHeight,
-//         //         this.frameWidth, this.frameHeight,
-//         //         this.x, this.y,
-//         //         this.w, this.h
-//         //     );
-//         // }
+        //va bajando de linea en los frame
+        if(this.countW > 3) {
+            this.countH ++;
+            this.countW = 0;
+        }
+        }
+        
+        if(this.countH > 3) {this.dispose = false;} // ha explotado la caja
+        console.log(this.countH)
+    }
     
+    // comprueba que ya no hay mas imagenes en el sprite y lo elimina
+    isVisible() {
+        return this.dispose;
+    }
 
-//     collides(obstaculo) {
-//             if (
-//                 this.x < obstaculo.x + obstaculo.w &&
-//                 this.y < obstaculo.y + obstaculo.h &&
-//                 this.x + this.w > obstaculo.x &&
-//                 this.y + this.h > obstaculo.y
-//             ) {
-//                 // Hay colisión con el jugador, inicia la animación del sprite
-//                 // this.animacion()
-//             }
-//         }
-// }
+    collides(obstaculo) {
+            if (
+                this.x < obstaculo.x + obstaculo.w &&
+                this.y < obstaculo.y + obstaculo.h &&
+                this.x + this.w > obstaculo.x &&
+                this.y + this.h > obstaculo.y
+            ) {
+                // Hay colisión con el jugador, inicia la animación del sprite
+                // this.animacion()
+            }
+        }
+}
 
 
 
@@ -187,96 +175,96 @@
 
 
 
-class Caja {
-    constructor(ctx) {
-        this.ctx = ctx;
-        this.x = 150;
-        this.y = 50;
-        this.w = 40; // Ancho del sprite (ajusta según tus necesidades)
-        this.h = 40; // Alto del sprite (ajusta según tus necesidades)
-        this.vx = 0;
-        this.vy = 0;
-        this.spriteSpeed = 0.2; // Velocidad del sprite
-        this.frameActual = 0;
-        this.numColumnas = 4; // Número de columnas en el sprite
-        this.numFilas = 3; // Número de filas en el sprite
-        this.frameWidth = 0; // Ancho de cada frame en el sprite
-        this.frameHeight = 0; // Alto de cada frame en el sprite
-        this.explosionFrames = 12; // Número total de frames en la animación de explosión
+// class Caja {
+//     constructor(ctx) {
+//         this.ctx = ctx;
+//         this.x = 150;
+//         this.y = 50;
+//         this.w = 40; // Ancho del sprite (ajusta según tus necesidades)
+//         this.h = 40; // Alto del sprite (ajusta según tus necesidades)
+//         this.vx = 0;
+//         this.vy = 0;
+//         this.spriteSpeed = 0.2; // Velocidad del sprite
+//         this.frameActual = 0;
+//         this.numColumnas = 4; // Número de columnas en el sprite
+//         this.numFilas = 3; // Número de filas en el sprite
+//         this.frameWidth = 0; // Ancho de cada frame en el sprite
+//         this.frameHeight = 0; // Alto de cada frame en el sprite
+//         this.explosionFrames = 12; // Número total de frames en la animación de explosión
 
-        this.explosionSpeed = 0.3; // Velocidad de la animación de explosión (ajusta según tus necesidades)
-        this.explosionCounter = 0; // Contador para controlar la velocidad de la animación
+//         this.explosionSpeed = 0.3; // Velocidad de la animación de explosión (ajusta según tus necesidades)
+//         this.explosionCounter = 0; // Contador para controlar la velocidad de la animación
 
-        // Carga la imagen del sprite
-        this.caja = new Image();
-        this.caja.src = "../Public/img/cajaA.png";
+//         // Carga la imagen del sprite
+//         this.caja = new Image();
+//         this.caja.src = "../Public/img/cajaA.png";
 
-        // Calcula el ancho y alto de cada frame en el sprite
-        this.caja.onload = () => {
-            this.frameWidth = this.caja.width / this.numColumnas;
-            this.frameHeight = this.caja.height / this.numFilas;
-        };
-    }
+//         // Calcula el ancho y alto de cada frame en el sprite
+//         this.caja.onload = () => {
+//             this.frameWidth = this.caja.width / this.numColumnas;
+//             this.frameHeight = this.caja.height / this.numFilas;
+//         };
+//     }
 
-    draw() {
-        // Calcular las coordenadas de textura
-        const fila = Math.floor(this.frameActual / this.numColumnas);
-        const columna = this.frameActual % this.numColumnas;
+//     draw() {
+//         // Calcular las coordenadas de textura
+//         const fila = Math.floor(this.frameActual / this.numColumnas);
+//         const columna = this.frameActual % this.numColumnas;
 
-        // Dibujar la subimagen actual del sprite en el canvas
-        this.ctx.drawImage(
-            this.caja,
-            columna * this.frameWidth, fila * this.frameHeight,
-            this.frameWidth, this.frameHeight,
-            this.x, this.y,
-            this.w, this.h
-        );
+//         // Dibujar la subimagen actual del sprite en el canvas
+//         this.ctx.drawImage(
+//             this.caja,
+//             columna * this.frameWidth, fila * this.frameHeight,
+//             this.frameWidth, this.frameHeight,
+//             this.x, this.y,
+//             this.w, this.h
+//         );
 
-    //   console.log(JSON.stringify(this.caja, null, 2)); //Esto imprimirá el objeto de manera más legible en la consola con una sangría de 2 espacios.
+//     //   console.log(JSON.stringify(this.caja, null, 2)); //Esto imprimirá el objeto de manera más legible en la consola con una sangría de 2 espacios.
 
-    }
+//     }
 
-    //!explode puede ajustarse la velocidad de la animacion y animate no, pero hacen la misma animacion. usar uno dentro de la cosision
-    explode() {
-        // Realizar la animación de explosión
-        if (this.explosionCounter % Math.floor(1 / this.explosionSpeed) === 0) {
-            // Cambiar a la siguiente frame en la animación de explosión
-            this.frameActual = (this.frameActual + 1) % this.explosionFrames;
-        }
-        this.explosionCounter++;
-        //hay que hacer que desaparezca la caja
-        // if (this.explosionCounter === explosionFrames){
-        //     this.caja = -10000;
-        // }
-    }
+//     //!explode puede ajustarse la velocidad de la animacion y animate no, pero hacen la misma animacion. usar uno dentro de la cosision
+//     explode() {
+//         // Realizar la animación de explosión
+//         if (this.explosionCounter % Math.floor(1 / this.explosionSpeed) === 0) {
+//             // Cambiar a la siguiente frame en la animación de explosión
+//             this.frameActual = (this.frameActual + 1) % this.explosionFrames;
+//         }
+//         this.explosionCounter++;
+//         //hay que hacer que desaparezca la caja
+//         // if (this.explosionCounter === explosionFrames){
+//         //     this.caja = -10000;
+//         // }
+//     }
     
-    animacion() {
-            // Incrementa el frame actual (puedes ajustar la velocidad de la animación cambiando el valor 0.2)
-            this.currentFrame += 0.2;
+//     animacion() {
+//             // Incrementa el frame actual (puedes ajustar la velocidad de la animación cambiando el valor 0.2)
+//             this.currentFrame += 0.2;
 
-            // Si hemos llegado al final del sprite, reinicia el frame
-            if (this.currentFrame >= this.totalFrames) {
-                this.currentFrame = 0;
-            }
+//             // Si hemos llegado al final del sprite, reinicia el frame
+//             if (this.currentFrame >= this.totalFrames) {
+//                 this.currentFrame = 0;
+//             }
 
-            // Actualiza la fila y la columna del frame actual
-            this.fila = Math.floor(this.currentFrame / this.totalFramesPerRow);
-            this.columna = this.currentFrame % this.totalFramesPerRow;
-        }
+//             // Actualiza la fila y la columna del frame actual
+//             this.fila = Math.floor(this.currentFrame / this.totalFramesPerRow);
+//             this.columna = this.currentFrame % this.totalFramesPerRow;
+//         }
 
 
 
-    collides(player) {
-        if (
-            this.x < player.x + player.w &&
-            this.y < player.y + player.h &&
-            this.x + this.w > player.x &&
-            this.y + this.h > player.y
-        ) {
-            // Hay colisión con el jugador, inicia la animación del sprite
-            this.explode()
-        }
-    }
-}
+//     collides(player) {
+//         if (
+//             this.x < player.x + player.w &&
+//             this.y < player.y + player.h &&
+//             this.x + this.w > player.x &&
+//             this.y + this.h > player.y
+//         ) {
+//             // Hay colisión con el jugador, inicia la animación del sprite
+//             this.explode()
+//         }
+//     }
+// }
 
 
--- Game/game.js
+++ Game/game.js
@@ -3,9 +3,10 @@ class Game {
         this.ctx = ctx;
         this.player = new Player(ctx);
         this.background = new Background(ctx);
-        this.caja = new Caja(ctx);
         this.interval = null;
         this.setListeners();
+        this.cajas = [];
+        this.tick = 0;
     }
 
     start() {
@@ -14,6 +15,19 @@ class Game {
             this.move();
             this.draw();
             this.checkCollision();
+            this.tick ++;
+            if(this.tick >= 200) {
+                this.addCaja();
+                this.tick = 0;
+            }
+
+            console.log(this.cajas)
+        //     for (let i = 0; i >= 50; i++) {
+        //         console.log("iteracion" + i)
+        //         let caja = new Caja(ctx, (100 + i*10));
+        //         this.cajas.push(caja);
+        //         i = 0;
+        //     }
         }, 1000 / 60)
 
         // Llama a requestAnimationFrame para repetir el bucle en el próximo frame
@@ -23,27 +37,34 @@ class Game {
     stop() {
         clearInterval(this.interval);
     }
-
+*
     clear() {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.cajas = this.cajas.filter(caja => caja.isVisible()) // si ha explotado lo elimina del array
+        
     }
 
     draw() {
         this.background.draw();
         this.player.draw();
-        this.caja.draw();
+
+        this.cajas.forEach(element => {
+            element.draw();
+        });
     
     }
 
     move() {
         this.player.move();
-
-
+        
+        this.cajas.forEach(element => {
+            element.move();
+        });
     }
 
     checkCollision() {
-        this.player.collides(this.caja);
-        this.caja.collides(this.player);
+        // this.player.collides(this.caja);
+        // this.caja.collides(this.player);
     }
 
     setListeners() { //permite hacer keyup y keydown para usar teclado para mover el personaje
@@ -53,6 +74,11 @@ class Game {
         document.addEventListener("keyup", (ev) => {
             this.player.keyUp(ev.keyCode);
         });
+    }
+
+    addCaja() {
+        let caja = new Caja(ctx);
+        this.cajas.push(caja);
     }
 
 
